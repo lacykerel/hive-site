d6bdec91fbc1c19c6729ebcee905d7244bce6e34
Jesse Bulpitt <jesse.bulpitt@gmail.com>
2015-12-16 15:29:46 -0600

    removing cap files

--- config/deploy.rb
+++
@@ -1,7 +0,0 @@
-# config valid only for Capistrano 3.1
-lock '>=3.2.1'
-
-set :application, 'greatbatch'
-set :repo_url, 'git@github.com:sq1agency/greatbatch.git'
-
-set :linked_dirs, %w{wp-content/uploads}
--- config/deploy/production.rb
+++
@@ -1,8 +0,0 @@
-server 'TBD', user: "ec2-user", roles: %w{app}
-
-set :branch, "master"
-set :deploy_to, "/var/www/production/greatbatch"
-
-set :ssh_options, {
-  forward_agent: true,
-}
