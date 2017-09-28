e73e018d3dcad923417f820f9d634a64f3f8bbde
Kerel Lacy <yakkez@gmail.com>
2017-09-28 01:29:09 -0500

    config

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,4 +1,17 @@
-server '165.227.94.190', user: "kerel", roles: %w{app}
+server '165.227.94.190', user: "root", roles: %w{app}
 
 set :branch, "master"
 set :deploy_to, "/var/www/html"
+
+set :ssh_options, {
+  forward_agent: true,
+}
+
+namespace :deploy do
+    task :config_file do
+      on roles :all do
+	    execute "rm -r #{release_path}/wp-config.php"
+        execute "mv #{release_path}/wp-config_prod.php #{release_path}/wp-config.php"
+      end
+    end
+end
